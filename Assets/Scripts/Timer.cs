2d3030fffda949afe5f9723e41be782ebb676b7c
mattcmerritt <mattcmerritt@gmail.com>
2021-04-15 16:16:11 -0400

    added Timer script, currently a MonoBehaviour

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,7 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer 
+public class Timer : MonoBehaviour
 {
-    
+    private double interval, elapsedTime;
+    private bool timerUp, isActive;
+
+    public Timer(double interval) 
+    {
+        this.interval = interval;
+    }
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= interval) 
+            {
+                timerUp = true;
+            }
+        }
+    }
+
+    public void Start() 
+    {
+        elapsedTime = 0;
+        timerUp = false;
+    }
+
+    public bool TimerFinished() 
+    {
+        if (timerUp) 
+        {
+            timerUp = false;
+            Start(); // restart the timer
+            return true;
+        } 
+        else 
+        {
+            return false;
+        }
+    }
+
 }
