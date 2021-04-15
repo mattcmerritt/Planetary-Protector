c699995529e5772c5a51205d37805cc9995b11fc
mattcmerritt <mattcmerritt@gmail.com>
2021-04-15 15:41:41 -0400

    added scene and placeholder ship sprite

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private void Start()
+    {
+        // create and start a timer
+        // get a first position to go to
+    }
+
+    private void Update()
+    {
+        // go to new position
+        // rotate to new position
+        // fire if timer countdown is up
+    }
+}
