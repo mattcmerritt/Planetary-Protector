eb5882e249769fbecfca38898ff1dc049f7dbf41
mikepmerritt <mikepmerritt@gmail.com>
2021-04-15 19:24:44 -0400

    added random planet spawning along a radius

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public float DistanceFromShip;
+    private float XDisplacement, YDisplacement;
+
+    private void Start()
+    {
+        XDisplacement = Random.Range(-DistanceFromShip, DistanceFromShip);
+        if(Random.Range(-1f, 1f) > 0)
+        {
+            YDisplacement = Mathf.Sqrt(Mathf.Pow(DistanceFromShip, 2) - Mathf.Pow(XDisplacement, 2));
+        }
+        else
+        {
+            YDisplacement = -Mathf.Sqrt(Mathf.Pow(DistanceFromShip, 2) - Mathf.Pow(XDisplacement, 2));
+        }
+        transform.position = new Vector3(XDisplacement, YDisplacement, 0);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
